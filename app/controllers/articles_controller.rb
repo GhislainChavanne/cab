e2a4718a9c4234b4c5c14e0eef7d3d8965d2349f
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-03 18:46:29 +0200

    model articles ok

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+end
