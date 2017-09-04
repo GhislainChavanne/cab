eeabb652ad085050c5172e2043b54697c0e27c22
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-04 23:52:06 +0200

    better

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
     @articles = Article.order('created_at DESC').first(4)
