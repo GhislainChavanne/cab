dcd0cb32735254384ba15a0608bd7461015a7cdf
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-06 12:15:18 +0200

    better contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :new, :create ]
+
   def new
     @contact = Contact.new
   end
@@ -11,7 +12,7 @@ class ContactsController < ApplicationController
       flash.now[:notice] = 'Merci pour votre message, Chavanne & Witt Avocats vous repondra dans les plus brefs délais !'
     else
       flash.now[:error] = "Le message n'a pas pu être envoyé"
-      render :create
+      render :new
     end
   end
 end
