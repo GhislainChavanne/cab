d3cd6d4b78202fe33944b4110f005e3ad1c0e3f7
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-06 09:14:38 +0200

    user ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users, only: [:new]
+  devise_for :users
   root to: 'contacts#new'
   resources :articles, only: [:index, :show]
   match '/contacts',     to: 'contacts#new',             via: 'get'
