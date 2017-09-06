5d15a9b0931ee5ca8d5c28afbf7b063119fe752a
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-06 16:35:33 +0200

    scroll navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
