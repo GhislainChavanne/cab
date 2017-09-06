9ceec5fad96b262c940c1eb43c1f0aaf1a195895
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-06 16:53:56 +0200

    better navbar and fixed btn articles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,13 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+$( document ).ready(function() {
+  $(document).on('click', '#btn', function(event){
+      event.preventDefault();
+
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+  });
 });
