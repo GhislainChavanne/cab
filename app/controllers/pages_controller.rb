d456ec9438e4b80c0a934c9e343a3a1f32a434bc
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-07 09:32:31 +0200

    better title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :contact ]
 
   def home
-    @articles = Article.order('created_at DESC').first(4)
+    @articles = Article.order('date DESC').first(4)
     @contact = Contact.new
   end
 
