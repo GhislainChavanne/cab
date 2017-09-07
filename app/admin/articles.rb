655cf0e377817d362edaa91f0ecab347c7a3e619
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-07 09:40:44 +0200

    datepicker ok

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,5 +1,25 @@
 
 ActiveAdmin.register Article do
   permit_params :title, :content, :date
+
+  menu label: "1. ACTUS"
+
+  index do
+    selectable_column
+    column :title
+    column :content
+    column :date
+    actions
+  end
+
+  form do |f|
+    inputs 'Details' do
+      input :title, placeholder: "Titre"
+      input :content, placeholder: "Contenu"
+      input :date, :as => :datepicker, :html_option => { value: Time.now }
+      f.actions
+    end
+  end
+
 end
 
