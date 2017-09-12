028291f7c784a19fb0ae20264ddd904661f59d89
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-12 13:26:55 +0200

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
     :address => 'smtp.gmail.com',
     :port => '587',
     :authentication => :plain,
-    :user_name => "gh.chavanne@gmail.com",
-    :password => "password",
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
