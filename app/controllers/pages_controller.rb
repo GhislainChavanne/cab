d73ab4d2e36f1c632a9e0a932c6cd21cdf27b188
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-09-12 13:32:55 +0200

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :contact ]
+  skip_before_action :authenticate_user!
 
   def home
     @articles = Article.order('date DESC').first(4)
