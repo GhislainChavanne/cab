8194b8ae203649905e1469125db3b5204d397960
Philippine Berton <Phi.berton@hotmail.fr>
2020-04-30 11:07:08 +0200

    Add Google Analytics & require email for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,10 @@
 class Contact < MailForm::Base
   include MailForm::Delivery
   attribute :nom,      :validate => true, :message => "blah blah blah"
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :sujet,      :validate => true
-  attribute :message,     :validate => true
-  attribute :nickname,  :captcha  => true
+  attribute :email,    :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, :presence => true
+  attribute :sujet,    :validate => true
+  attribute :message,  :validate => true
+  attribute :nickname, :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
