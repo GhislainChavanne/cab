9203b5c73eb608cfdd29b56d2c8ef20fdf8ddaae
Philippine Berton <Phi.berton@hotmail.fr>
2020-08-30 12:32:50 +0200

    delete ES6

--- app/assets/javascripts/scroll.js
+++
@@ -1,8 +0,0 @@
-const chevron = document.getElementById("chevron");
-const presentation = document.getElementById("presentation");
-
-if(chevron) {
-  chevron.addEventListener("click", (event) => {
-    presentation.scrollIntoView( {behavior: "smooth"} );
-  });
-};
