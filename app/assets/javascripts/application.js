d7cc2bf1af2f37bb31c312cd7302f7e6aa05109e
Romain Sanson <romain.sanson@hey.com>
2024-03-03 11:36:42 +0100

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,23 @@
-
-
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
 //= require bootstrap.min.js
 
+$(document).ready(function () {
+  $(document).on('click', '.scroll-to', function (event) {
+    event.preventDefault();
+    var offset = -70;
 
-$( document ).ready(function() {
-  $(document).on('click', '#btn', function(event){
-      event.preventDefault();
-      var offset = - 70;
-
-      $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top + offset
-      }, 500);
+    $('html, body').animate(
+      {
+        scrollTop: $($.attr(this, 'href')).offset().top + offset,
+      },
+      500
+    );
   });
 });
 
 $(function () {
-  $('[data-toggle="popover"]').popover({html:true})
-})
-
+  $('[data-toggle="popover"]').popover({ html: true });
+});
