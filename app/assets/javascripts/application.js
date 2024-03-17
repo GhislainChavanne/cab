ee6689408c347b02e09739a0402e50d24e8e0366
Romain Sanson <romain.sanson@hey.com>
2024-03-17 15:04:39 +0100

    clean

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,9 @@
 //= require_tree .
 //= require bootstrap.min.js
 
-const initBurger = () => {
-  const btn = document.querySelector('#burger-btn');
-  const menu = document.querySelector('#mobile-nav');
+var initBurger = () => {
+  var btn = document.querySelector('#burger-btn');
+  var menu = document.querySelector('#mobile-nav');
 
   btn.addEventListener('click', (event) => {
     menu.classList.toggle('hidden');
